6eb21d78a68bf933005a1f1d62c919920e442c2f
luke3481 <luke.wesley.birch@gmail.com>
2023-04-03 13:17:54 -0500

    Revert "added hamburger menu"
    This reverts commit 2fbea928daaafda0ece7f78b4be3a06f68b4430d.

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -14,9 +14,6 @@ import lukePhoto from "../imgs/Luke.jpeg";
 import willPhoto from "../imgs/Will.jpeg";
 import billPhoto from "../imgs/Bill.jpeg";
 import treyPhoto from "../imgs/Trey.jpeg";
-import weiPhoto from "../imgs/Wei.jpeg";
-import darrenPhoto from "../imgs/Darren.jpeg";
-import johnPhoto from "../imgs/John.jpg";
 
 const members = [
   {
@@ -44,12 +41,6 @@ const members = [
     linkedin: "https://www.linkedin.com/in/bill-lennon/",
   },
   {
-    name: "John Hart",
-    photo: johnPhoto,
-    position: "Business Development",
-    linkedin: "https://www.linkedin.com/in/john-hart-33435514b/",
-  },
-  {
     name: "Trey Grizzard",
     photo: treyPhoto,
     position: "Engineering",
@@ -60,18 +51,6 @@ const members = [
     photo: willPhoto,
     position: "Engineering",
     linkedin: "https://www.linkedin.com/in/willschwieder/",
-  },
-  {
-    name: "Darren Colby",
-    photo: darrenPhoto,
-    position: "Engineering",
-    linkedin: "https://www.linkedin.com/in/darren-colby/",
-  },
-  {
-    name: "Wei Cai",
-    photo: weiPhoto,
-    position: "Engineering",
-    linkedin: "https://linkedin.com/in/wei-cai-5b7159230/",
   },
 ];
 
